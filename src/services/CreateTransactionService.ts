2f557a976f2f9f9803f6d2dbea8e8d8ac9680f5a
Gabriel Porfirio <gabrielporfirio@Gabriels-MacBook-Air.local>
2020-07-08 04:24:18 -0300

    resolvendo

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO{
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,9 +14,15 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value}: RequestDTO): Transaction {
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value
+    });
+    return transaction;
   }
 }
+
 
 export default CreateTransactionService;
